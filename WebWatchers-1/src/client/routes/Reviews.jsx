11a209a35e55c538c304fd07ee910de01f2e7d36
Annorino <132631480+Annorinoo@users.noreply.github.com>
2023-10-08 00:07:02 -0700

    changes to Reviews.jsx and added a searchbar

--- WebWatchers-1/src/client/routes/Reviews.jsx
+++ WebWatchers-1/src/client/routes/Reviews.jsx
@@ -1,44 +1,103 @@
-import { useState, useEffect } from "react"
-import { fetchAllReviews, deleteReview } from "../API/ajaxHelper";
+// import { useState, useEffect } from "react"
+// import { fetchAllReviews, deleteReview } from "../API/ajaxHelper";
+
+// export default function Reviews() {
+//     const [reviews, setReviews] = useState([]);
+
+//     function renderAllReviews() {
+//         return reviews.map((review) => {
+//             return (
+//                 <div key={review?.id} className="reviews">
+//                     <h1 className="review-name">{review?.name}</h1>
+//                     <h3 className="review-content">{review?.content}</h3>
+//                     <h3 className="review-rating">{review?.rating}</h3>
+//                     <h3 className="review-date">{review?.date}</h3>
+//                     <button className="review-button" onClick={() => handleDelete(review.id)}>Delete</button>
+//                 </div>
+//             )
+//         })
+//     }
+
+//     useEffect(() => {
+//         async function allReviewsHandler() {
+//             const result = await fetchAllReviews();
+//             console.log({result});
+//             setReviews(result.reviews);
+//         } allReviewsHandler();
+//     }, [])
+
+//     async function handleDelete(reviewId) {
+//         try {
+//             await deleteReview(reviewId);
+//             const updatedReviews = await fetchAllReviews();
+//             setReviews(updatedReviews.reviews);
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     }
+
+//     return (
+//         <div className="all-reviews">
+//             {renderAllReviews()}
+//         </div>
+//     )
+// }
+
+import React, { useState, useEffect } from "react";
+import { fetchAllReviews } from "../API/ajaxHelper";
 
 export default function Reviews() {
-    const [reviews, setReviews] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    function renderAllReviews() {
-        return reviews.map((review) => {
-            return (
-                <div key={review?.id} className="reviews">
-                    <h1 className="review-name">{review?.name}</h1>
-                    <h3 className="review-content">{review?.content}</h3>
-                    <h3 className="review-rating">{review?.rating}</h3>
-                    <h3 className="review-date">{review?.date}</h3>
-                    <button className="review-button" onClick={() => handleDelete(review.id)}>Delete</button>
-                </div>
-            )
-        })
-    }
+  // Function to handle changes in the search input
+  function handleSearchInputChange(event) {
+    setSearchTerm(event.target.value);
+  }
 
-    useEffect(() => {
-        async function allReviewsHandler() {
-            const result = await fetchAllReviews();
-            console.log({result});
-            setReviews(result.reviews);
-        } allReviewsHandler();
-    }, [])
+  // Function to render filtered reviews
+  function renderAllReviews() {
+    const filteredReviews = reviews.filter((review) => {
+      const lowercasedName = review.name.toLowerCase();
+      const lowercasedContent = review.content.toLowerCase();
+      const lowercasedSearchTerm = searchTerm.toLowerCase();
+      return (
+        lowercasedName.includes(lowercasedSearchTerm) ||
+        lowercasedContent.includes(lowercasedSearchTerm)
+      );
+    });
 
-    async function handleDelete(reviewId) {
-        try {
-            await deleteReview(reviewId);
-            const updatedReviews = await fetchAllReviews();
-            setReviews(updatedReviews.reviews);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    return (
-        <div className="all-reviews">
-            {renderAllReviews()}
+    return filteredReviews.map((review) => {
+      return (
+        <div key={review?.id} className="reviews">
+          <h1 className="review-name">{review?.name}</h1>
+          <h3 className="review-content">{review?.content}</h3>
+          <h3 className="review-rating">{review?.rating}</h3>
+          <h3 className="review-date">{review?.date}</h3>
         </div>
-    )
+      );
+    });
+  }
+
+  useEffect(() => {
+    async function allReviewsHandler() {
+      const result = await fetchAllReviews();
+      console.log({ result });
+      setReviews(result.reviews);
+    }
+    allReviewsHandler();
+  }, []);
+
+  return (
+    <div className="all-reviews">
+      <h2>Reviews</h2>
+      <input
+        type="text"
+        placeholder="Search reviews..."
+        value={searchTerm}
+        onChange={handleSearchInputChange}
+      />
+      {renderAllReviews()}
+    </div>
+  );
 }
