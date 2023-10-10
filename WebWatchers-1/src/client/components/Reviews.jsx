6c77ba4a170c1a3d4a230dc895a0da5a8a3c5241
Annorino <132631480+Annorinoo@users.noreply.github.com>
2023-10-09 20:47:01 -0700

    added thumbs up and down, refining its logged in functions, implemented a 'must be logged in' alert if not logged in

--- WebWatchers-1/src/client/components/Reviews.jsx
+++ WebWatchers-1/src/client/components/Reviews.jsx
@@ -1,42 +1,249 @@
-import { useState, useEffect } from 'react'
-import { fetchAllReviews } from '../API/ajaxHelper.js';
+// v1.0
+// import React, { useState, useEffect } from "react";
+// import { fetchAllReviews, deleteReview } from "../API/ajaxHelper";
+// // import "../Style/Reviews.css";
 
-export default function ReviewSearch() {
+// export default function Reviews() {
+//     const [reviews, setReviews] = useState([]);
+//     const [thumbsUp, setThumbsUp] = useState({});
+//     const [thumbsDown, setThumbsDown] = useState({});
+//     const [searchQuery, setSearchQuery] = useState(""); // Step 1: Add state for the search query
+
+//     useEffect(() => {
+//         async function allReviewsHandler() {
+//             try {
+//                 const result = await fetchAllReviews();
+//                 setReviews(result.reviews);
+
+//                 // Initialize the thumbs up and thumbs down state for each review
+//                 const initialThumbsUpState = {};
+//                 const initialThumbsDownState = {};
+
+//                 result.reviews.forEach((review) => {
+//                     initialThumbsUpState[review.id] = 0;
+//                     initialThumbsDownState[review.id] = 0;
+//                 });
+
+//                 setThumbsUp(initialThumbsUpState);
+//                 setThumbsDown(initialThumbsDownState);
+//             } catch (error) {
+//                 console.error(error);
+//             }
+//         }
+//         allReviewsHandler();
+//     }, []);
+
+//     const handleThumbsUp = (reviewId) => {
+//         setThumbsUp((prevThumbsUp) => ({
+//             ...prevThumbsUp,
+//             [reviewId]: prevThumbsUp[reviewId] + 1,
+//         }));
+//     };
+
+//     const handleThumbsDown = (reviewId) => {
+//         setThumbsDown((prevThumbsDown) => ({
+//             ...prevThumbsDown,
+//             [reviewId]: prevThumbsDown[reviewId] + 1,
+//         }));
+//     };
+
+//     const renderAllReviews = () => {
+//         // Step 2: Modify this function to filter reviews based on the search query
+//         const filteredReviews = reviews.filter((review) =>
+//             review.name.toLowerCase().includes(searchQuery.toLowerCase())
+//         );
+
+//         return filteredReviews.map((review) => (
+//             <div key={review.id} className="review">
+//                 <h1 className="review-name">{review.name}</h1>
+//                 <h3 className="review-content">{review.content}</h3>
+//                 <h3 className="review-rating">{review.rating}</h3>
+//                 <h3 className="review-date">{review.date}</h3>
+//                 <div className="review-actions">
+//                     <button onClick={() => handleThumbsUp(review.id)}>
+//                         Thumbs Up ({thumbsUp[review.id]})
+//                     </button>
+//                     <button onClick={() => handleThumbsDown(review.id)}>
+//                         Thumbs Down ({thumbsDown[review.id]})
+//                     </button>
+//                     <button onClick={() => handleDelete(review.id)}>Delete</button>
+//                 </div>
+//             </div>
+//         ));
+//     };
+
+//     async function handleDelete(reviewId) {
+//         try {
+//             await deleteReview(reviewId);
+//             const updatedReviews = await fetchAllReviews();
+//             setReviews(updatedReviews.reviews);
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     }
+
+//     return (
+//         <div className="all-reviews">
+//             <input
+//                 type="text"
+//                 placeholder="Search reviews"
+//                 value={searchQuery}
+//                 onChange={(e) => setSearchQuery(e.target.value)} // Step 4: Attach onChange event handler
+//             />
+//             {renderAllReviews()}
+//         </div>
+//     );
+// }
+
+//v1.1
+import React, { useState, useEffect } from "react";
+import { fetchAllReviews, deleteReview } from "../API/ajaxHelper";
+// import "../Style/Reviews.css";
+
+export default function Reviews({ user }) {
     const [reviews, setReviews] = useState([]);
-    const [state, setState] = useState({query: '', list: []})
-
-    const handleChange = async (e) => {
-        const results = reviews.data.reviews.filter(review => {
-            if (e.target.value === '') return reviews
-            return review.title.toLowerCase().includes(e.target.value.toLowerCase())
-        })
-        setState({
-            query: e.target.value,
-            list: results
-        })
-    }
+    const [thumbsUp, setThumbsUp] = useState({});
+    const [thumbsDown, setThumbsDown] = useState({});
+    const [searchQuery, setSearchQuery] = useState("");
 
     useEffect(() => {
-        async function allReviewHandler() {
-            const result = await fetchAllReviews();
-            setReviews(result);
-        } allReviewHandler();
-    }, [])
+        async function allReviewsHandler() {
+            try {
+                const result = await fetchAllReviews();
+                setReviews(result.reviews);
+
+                // Initialize the thumbs up and thumbs down state for each review
+                const initialThumbsUpState = {};
+                const initialThumbsDownState = {};
+
+                result.reviews.forEach((review) => {
+                    initialThumbsUpState[review.id] = 0;
+                    initialThumbsDownState[review.id] = 0;
+                });
+
+                setThumbsUp(initialThumbsUpState);
+                setThumbsDown(initialThumbsDownState);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        allReviewsHandler();
+    }, []);
+
+    const handleThumbsUp = (reviewId) => {
+        if (!user) {
+            // User not logged in, show a message or prevent voting
+            alert("You must be logged in to vote.");
+            return;
+        }
+
+        if (hasUserVoted(user.id, reviewId, "thumbsUp")) {
+            // User has already voted, show a message or prevent voting
+            alert("You have already voted on this review.");
+            return;
+        }
+
+        setThumbsUp((prevThumbsUp) => ({
+            ...prevThumbsUp,
+            [reviewId]: prevThumbsUp[reviewId] + 1,
+        }));
+
+        // Remember that the user has voted on this review
+        markUserVoted(user.id, reviewId, "thumbsUp");
+    };
+
+    const handleThumbsDown = (reviewId) => {
+        if (!user) {
+            // User not logged in, show a message or prevent voting
+            alert("You must be logged in to vote.");
+            return;
+        }
+
+        if (hasUserVoted(user.id, reviewId, "thumbsDown")) {
+            // User has already voted, show a message or prevent voting
+            alert("You have already voted on this review.");
+            return;
+        }
+
+        setThumbsDown((prevThumbsDown) => ({
+            ...prevThumbsDown,
+            [reviewId]: prevThumbsDown[reviewId] + 1,
+        }));
+
+        // Remember that the user has voted on this review
+        markUserVoted(user.id, reviewId, "thumbsDown");
+    };
+
+    // Helper functions to check and record user votes
+    const hasUserVoted = (userId, reviewId, voteType) => {
+        // You can implement this logic using local storage or a server-side database
+        // For example, you can store user votes in local storage as a JSON object
+        // and check if the user has already voted for a specific review.
+        const userVotes = JSON.parse(localStorage.getItem("userVotes")) || {};
+
+        if (userVotes[userId] && userVotes[userId][reviewId]) {
+            return userVotes[userId][reviewId] === voteType;
+        }
+
+        return false;
+    };
+
+    const markUserVoted = (userId, reviewId, voteType) => {
+        // You can implement this logic to record user votes in local storage
+        // or send a request to the server to update the user's vote in a database.
+        const userVotes = JSON.parse(localStorage.getItem("userVotes")) || {};
+
+        if (!userVotes[userId]) {
+            userVotes[userId] = {};
+        }
+
+        userVotes[userId][reviewId] = voteType;
+        localStorage.setItem("userVotes", JSON.stringify(userVotes));
+    };
+
+    const renderAllReviews = () => {
+        const filteredReviews = reviews.filter((review) =>
+            review.name.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+
+        return filteredReviews.map((review) => (
+            <div key={review.id} className="review">
+                <h1 className="review-name">{review.name}</h1>
+                <h3 className="review-content">{review.content}</h3>
+                <h3 className="review-rating">{review.rating}</h3>
+                <h3 className="review-date">{review.date}</h3>
+                <div className="review-actions">
+                    <button onClick={() => handleThumbsUp(review.id)}>
+                        Thumbs Up ({thumbsUp[review.id]})
+                    </button>
+                    <button onClick={() => handleThumbsDown(review.id)}>
+                        Thumbs Down ({thumbsDown[review.id]})
+                    </button>
+                    <button onClick={() => handleDelete(review.id)}>Delete</button>
+                </div>
+            </div>
+        ));
+    };
+
+    async function handleDelete(reviewId) {
+        try {
+            await deleteReview(reviewId);
+            const updatedReviews = await fetchAllReviews();
+            setReviews(updatedReviews.reviews);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
-        <div className='searchbar'>
-            <h1 className='searchbarHeader'>Search for a post!</h1>
-            <h3 className='searchbarFooter'>It will find post using letters typed</h3>
-            <form className='searchForm'>
-                <label className='searchbarText'> Searchbar
-                    <input className='searchbarInfo' onChange={handleChange} value={state.query} type='search' placeholder='Search for an item' />
-                </label>
-            </form>
-            <ul>
-                {(state.query === '' ? '' : state.list.map(review => {
-                    return <li key={review.title}>{review.title}</li>
-                }))}
-            </ul>
+        <div className="all-reviews">
+            <input
+                type="text"
+                placeholder="Search reviews"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            {renderAllReviews()}
         </div>
-    )
+    );
 }
--- WebWatchers-1/src/client/routes/Reviews.jsx
+++ WebWatchers-1/src/client/routes/Reviews.jsx
@@ -1,103 +1,12 @@
-// import { useState, useEffect } from "react"
-// import { fetchAllReviews, deleteReview } from "../API/ajaxHelper";
+import React from 'react';
+// import ReviewSearch from '../components/ReviewSearch';
+import Reviews from '../components/Reviews';
 
-// export default function Reviews() {
-//     const [reviews, setReviews] = useState([]);
-
-//     function renderAllReviews() {
-//         return reviews.map((review) => {
-//             return (
-//                 <div key={review?.id} className="reviews">
-//                     <h1 className="review-name">{review?.name}</h1>
-//                     <h3 className="review-content">{review?.content}</h3>
-//                     <h3 className="review-rating">{review?.rating}</h3>
-//                     <h3 className="review-date">{review?.date}</h3>
-//                     <button className="review-button" onClick={() => handleDelete(review.id)}>Delete</button>
-//                 </div>
-//             )
-//         })
-//     }
-
-//     useEffect(() => {
-//         async function allReviewsHandler() {
-//             const result = await fetchAllReviews();
-//             console.log({result});
-//             setReviews(result.reviews);
-//         } allReviewsHandler();
-//     }, [])
-
-//     async function handleDelete(reviewId) {
-//         try {
-//             await deleteReview(reviewId);
-//             const updatedReviews = await fetchAllReviews();
-//             setReviews(updatedReviews.reviews);
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     }
-
-//     return (
-//         <div className="all-reviews">
-//             {renderAllReviews()}
-//         </div>
-//     )
-// }
-
-import React, { useState, useEffect } from "react";
-import { fetchAllReviews } from "../API/ajaxHelper";
-
-export default function Reviews() {
-  const [reviews, setReviews] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-
-  // Function to handle changes in the search input
-  function handleSearchInputChange(event) {
-    setSearchTerm(event.target.value);
-  }
-
-  // Function to render filtered reviews
-  function renderAllReviews() {
-    const filteredReviews = reviews.filter((review) => {
-      const lowercasedName = review.name.toLowerCase();
-      const lowercasedContent = review.content.toLowerCase();
-      const lowercasedSearchTerm = searchTerm.toLowerCase();
-      return (
-        lowercasedName.includes(lowercasedSearchTerm) ||
-        lowercasedContent.includes(lowercasedSearchTerm)
-      );
-    });
-
-    return filteredReviews.map((review) => {
-      return (
-        <div key={review?.id} className="reviews">
-          <h1 className="review-name">{review?.name}</h1>
-          <h3 className="review-content">{review?.content}</h3>
-          <h3 className="review-rating">{review?.rating}</h3>
-          <h3 className="review-date">{review?.date}</h3>
+export default function ReviewsPage() {
+    return (
+        <div>
+            {/* <ReviewSearch /> */}
+            <Reviews />   
         </div>
-      );
-    });
-  }
-
-  useEffect(() => {
-    async function allReviewsHandler() {
-      const result = await fetchAllReviews();
-      console.log({ result });
-      setReviews(result.reviews);
-    }
-    allReviewsHandler();
-  }, []);
-
-  return (
-    <div className="all-reviews">
-      <h2>Reviews</h2>
-      <input
-        type="text"
-        placeholder="Search reviews..."
-        value={searchTerm}
-        onChange={handleSearchInputChange}
-      />
-      {renderAllReviews()}
-    </div>
-  );
+    );
 }
